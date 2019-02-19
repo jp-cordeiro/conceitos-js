ade474bcacee603eb51093019e9185728c28ac34
jp <jpc.araujo@hotmail.com>
2019-02-18 23:07:39 -0300

    Funções com string

---
+++ strings.js
@@ -0,0 +1,6 @@
+let texto = "O One Piece é REAL"
+texto = texto.toLowerCase()
+console.log(texto.startsWith('o on'))
+console.log(texto.endsWith('real'))
+console.log(texto.includes('piece'))
+console.log('Luffy \n'.repeat(5))
