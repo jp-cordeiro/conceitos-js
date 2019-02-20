990e1d75c7f38e5fba445100aed0e27a5ebd154d
jp <jpc.araujo@hotmail.com>
2019-02-20 20:50:31 -0300

    iterator, iterable e for-of

--- iterator-for-of.js
+++ iterator-for-of.js
@@ -1,7 +1,19 @@
 let txt = 'Fortaleza' //Iterable
 let it = txt[Symbol.iterator](); //Iterator
 
-do{
-    console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+
+for (letra of txt){
+    console.log(letra)
+    //Com for of é possível realizar um ponto de parada
+    if(letra === 'a') break
 }
-while (!it.next().done)
