35f857408b379e9beb5269f7287511edf3c29340
jp <jpc.araujo@hotmail.com>
2019-02-20 23:15:04 -0300

    padStart-padEnd

---
+++ padStart-padEnd.js
@@ -0,0 +1,15 @@
+/*
+Coloca uma cadeia de caracteres no início da string
+ */
+let nome = "Paulo".padStart(15,'Teste')
+console.log(nome)
+
+/*
+Coloca uma cadeia de caracteres no final da string
+ */
+let sobrenome = "Silva".padEnd(15,'Teste')
+console.log(sobrenome)
+
+/*
+Todas baseando-se na quantidade de caracteres
+ */
